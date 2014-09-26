f6fd31d5cc11474df551b2c903c16592ad399423
esteban <esteban@med-mbp-esteban.globe.nemgint.com>
2014-09-26 18:02:02 -0400

    initial HorizBarcharts commit

---
+++ graphics/HorizBarcharts/js/main.js
@@ -0,0 +1,6 @@
+(function() { globe.onDefine('window.jQuery && $(".article-graphic.HorizBarcharts").length', function() {
+
+	var masterSelector = '.article-graphic.HorizBarcharts';
+	var master = $(masterSelector);
+
+}); }());
