db6dae8b09924338654998ee6de566d78a76e683
esteban <esteban@med-mbp-esteban.globe.nemgint.com>
2014-10-06 14:40:35 -0400

    initial StatesMap commit

---
+++ graphics/StatesMap/js/main.js
@@ -0,0 +1,6 @@
+(function() { globe.onDefine('window.jQuery && $(".article-graphic.StatesMap").length', function() {
+
+	var masterSelector = '.article-graphic.StatesMap';
+	var master = $(masterSelector);
+
+}); }());
