efeb3f007cd17c365d852ce6bc1ad2f639efd044
esteban <esteban@med-mbp-esteban.globe.nemgint.com>
2014-10-06 14:41:10 -0400

    initial FeverLine commit

---
+++ graphics/FeverLine/js/main.js
@@ -0,0 +1,6 @@
+(function() { globe.onDefine('window.jQuery && $(".article-graphic.FeverLine").length', function() {
+
+	var masterSelector = '.article-graphic.FeverLine';
+	var master = $(masterSelector);
+
+}); }());
