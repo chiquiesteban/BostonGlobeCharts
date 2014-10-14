d06e23c3dccddbd27cd71a911e7b993fb7c8b0ed
esteban <esteban@med-mbp-esteban.globe.nemgint.com>
2014-10-14 13:50:15 -0400

    initial DotMap commit

---
+++ graphics/DotMap/js/main.js
@@ -0,0 +1,6 @@
+(function() { globe.onDefine('window.jQuery && $(".article-graphic.DotMap").length', function() {
+
+	var masterSelector = '.article-graphic.DotMap';
+	var master = $(masterSelector);
+
+}); }());
