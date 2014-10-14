6891925d3f861e31c3fe65fe776a3eb7f43215f1
esteban <esteban@med-mbp-esteban.globe.nemgint.com>
2014-10-14 18:24:41 -0400

    initial Scatterplot commit

---
+++ graphics/Scatterplot/js/main.js
@@ -0,0 +1,6 @@
+(function() { globe.onDefine('window.jQuery && $(".article-graphic.Scatterplot").length', function() {
+
+	var masterSelector = '.article-graphic.Scatterplot';
+	var master = $(masterSelector);
+
+}); }());
