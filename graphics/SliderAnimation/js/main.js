42e8c7d643d6327a5274a50c09acc4b3c3a0d85d
chiquiesteban <chiquiesteban@gmail.com>
2014-10-17 18:26:22 -0400

    Uploaded the SliderAnimation template

--- graphics/SliderAnimation/js/main.js
+++ graphics/SliderAnimation/js/main.js
@@ -1,6 +1,83 @@
+var val, val2, percentage, whereiam, whereiam2,intmov, intmov2, intmovpct, intmovpct2;
+var whereileft2 = 0; 
+var whereileft = 0;
+
 (function() { globe.onDefine('window.jQuery && $(".article-graphic.SliderAnimation").length', function() {
 
 	var masterSelector = '.article-graphic.SliderAnimation';
 	var master = $(masterSelector);
 
+	for (var i in data) {
+	
+		if (i == 0) {
+			$('#bigimage').append('<img class="imagerotation" id="img'+i+'" src="'+data[i][0]+'">')
+		} else {
+			$('#bigimage').append('<img class="imagerotation" id="img'+i+'" style="display:none" src="'+data[i][0]+'">')
+		}
+	}	
+		
+	var imgw = $("#bigimage").width()
+	$("#bigimage").css("height", imgw*ratio); 
+	$("#bigimagery").css("height", imgw*ratio); 
+		
+	$(window).resize(function(){
+		imgw = $("#bigimage").width()
+		$("#bigimage").css("height", imgw*ratio); 
+		$("#bigimagery").css("height", imgw*ratio);
+	})
+	
+	detectmob();
+
 }); }());
+
+function detectmob() { 
+
+ 	var sumd = 100/data.length;
+
+	 if( navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i) ){
+	   	
+		$("#bigimage").bind("touchstart",function(e){whereiam = e.originalEvent.touches[0].pageX;})
+		
+		$("#bigimage").bind("touchmove",function(event){
+		  event.preventDefault();
+		  var e = event.originalEvent;  
+		  val = e.targetTouches[0].pageX;
+		  intmov = val-whereiam;
+		  intmovpct = ((intmov*100)/$("#bigimagery").width())+whereileft;
+		  if (intmovpct > 100){intmovpct = 100;}
+		  if (intmovpct < 0){intmovpct = 0;}    
+		  
+		  for (var h in data) {		 
+				if (intmovpct >= h*sumd && intmovpct < (h*sumd+sumd)) {
+				 $('.imagerotation').css("display", "none"); 
+				 $('#img'+h).css("display", "inline");	
+				 $('#text-container').empty().append(data[h][1]);
+				} 
+			 }
+				
+	   });   
+   
+   $("#bigimage").bind("touchend",function(event){whereileft = intmovpct;})  
+
+   $("#text-container").append("Swipe the image with your finger")
+  
+  } else {
+	 
+	  $("#text-container").append("Drag the slider")
+	
+		 $("#slider").slider()
+
+	 
+	 $("#slider").on("slide", function(event, ui) {
+	
+		 for (var k in data) {		 
+			if (ui.value >= k*sumd && ui.value < (k*sumd+sumd)) {
+			 $('.imagerotation').css("display", "none"); 
+			 $('#img'+k).css("display", "inline");	
+			 $('#text-container').empty().append(data[k][1]);
+			} 
+		 }
+	});
+	    
+  }
+}
---
+++ graphics/SliderAnimation/js/utils.js
@@ -0,0 +1,21 @@
+var ratio = 0.75;
+/*ASPECT RATIO OF THE IMAGES LOADED --- IF THEY ARE 800px * 600 px, THE RATIO IS 0.75, FOR 1000px * 1000px ratio=1*/
+
+/*In the first set of quotes, include the image you would like to show at each step. In the second one, the text ypu want to go with it.*/
+/*If you don't want any text, just leave the quotes empty "" */
+
+
+var data = [
+  	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/0.jpg", "Text 0"],
+	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/10.jpg", "Text 1"],
+	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/20.jpg", "Text 2"],
+	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/30.jpg", "Text 3"],
+	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/40.jpg", "Text 4"],
+	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/50.jpg", "Text 5"],
+	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/60.jpg", "Text 6"],
+	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/70.jpg", "Text 7"],
+	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/80.jpg", "Text 8"],
+	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/90.jpg", "Text 9"],
+	["http://www.bostonglobe.com/rw//Boston/2011-2020/WebGraphics/Sports/BostonGlobe.com/2013/03/fenway/img/100.jpg", "Text 10"]
+];
+/*INCLUDE AS MANY IMAGES AS YOU WANT TO*/
