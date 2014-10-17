99d6fb7a846b76a5401ee5ef64a2306040baac4d
chiquiesteban <chiquiesteban@gmail.com>
2014-10-17 16:35:06 -0400

    initial SliderAnimation commit

---
+++ graphics/SliderAnimation/js/main.js
@@ -0,0 +1,6 @@
+(function() { globe.onDefine('window.jQuery && $(".article-graphic.SliderAnimation").length', function() {
+
+	var masterSelector = '.article-graphic.SliderAnimation';
+	var master = $(masterSelector);
+
+}); }());
