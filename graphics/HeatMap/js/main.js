39f0a1fa37494ee3845d26ddc257b1aa041f90c5
chiquiesteban <chiquiesteban@gmail.com>
2014-10-24 12:25:16 -0400

    initial HeatMap commit

---
+++ graphics/HeatMap/js/main.js
@@ -0,0 +1,6 @@
+(function() { globe.onDefine('window.jQuery && $(".article-graphic.HeatMap").length', function() {
+
+	var masterSelector = '.article-graphic.HeatMap';
+	var master = $(masterSelector);
+
+}); }());
