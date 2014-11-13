ccae268a92e19b069ccdcf5c8d868ba6d4f9e87f
chiquiesteban <chiquiesteban@gmail.com>
2014-11-13 16:40:07 -0500

    initial StackedBarchart commit

---
+++ graphics/StackedBarchart/js/main.js
@@ -0,0 +1,6 @@
+(function() { globe.onDefine('window.jQuery && $(".article-graphic.StackedBarchart").length', function() {
+
+	var masterSelector = '.article-graphic.StackedBarchart';
+	var master = $(masterSelector);
+
+}); }());
