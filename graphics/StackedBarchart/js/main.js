4dee32be0ef34888a46611f8c0878a9104192880
chiquiesteban <chiquiesteban@gmail.com>
2014-11-14 13:53:58 -0500

    Firefox fixes for stackedBarcharts

--- graphics/StackedBarchart/js/main.js
+++ graphics/StackedBarchart/js/main.js
@@ -1,6 +1,7 @@
-var theconcept, pos, thex, they;
+var theconcept, pos, thex, they, largest;
 var nameNodes = [];
 var isFirefox = typeof InstallTrigger !== 'undefined';
+var totalNodes = [];
 
 (function() { globe.onDefine('window.jQuery && $(".article-graphic.StackedBarchart").length', function() {
 
@@ -19,17 +20,28 @@ var isFirefox = typeof InstallTrigger !== 'undefined';
 	}
 
 	for (var i in data) {
+		var theTotal = 0;
 		for (var j in nameNodes) {
-
 			if (j>0) {
+				theTotal  = theTotal+(data[i][nameNodes[j]]);
+			}
+		}
+		totalNodes.push(theTotal)
+	}
+	largest = Math.max.apply(Math, totalNodes);
 
-				$("#bar"+i).append('<div class="portion color'+j+'" id="id_'+i+"_"+j+'" style="width:'+data[i][nameNodes[j]]+'%;"><span>'+prefix+data[i][nameNodes[j]]+suffix+'</span></div>')
+	for (var i in data) {	
+		for (var j in nameNodes) {
+			if (j>0) {
+				$("#bar"+i).append('<div class="portion color'+j+'" id="id_'+i+"_"+j+'" style="width:'+(data[i][nameNodes[j]]*100)/largest+'%;"><span>'+prefix+data[i][nameNodes[j]]+suffix+'</span></div>')
 				if ($("#bar"+i+" .color"+j).width() < 50 ) {
 					$("#bar"+i+" .color"+j+" span").css("display", "none") 
 				}
 			}
-		}
+		}	
 	}
+	
+	
 
 	for (var i in nameNodes) {
 		if (i>0) {
@@ -46,9 +58,31 @@ var isFirefox = typeof InstallTrigger !== 'undefined';
     		showTag(this.id)
     	})
 	} else {   
-   		$('.portion').mouseover(function(){
-    		showTag(this.id)
-    	})
+		if (isFirefox == true) {
+			$('.portion').mouseover(function(e){
+    			$('#tag').remove();
+				$(".portion").css("-moz-box-shadow","inset 0 0 0 #000000").css("-webkit-box-shadow","inset 0 0 0 #000000").css("box-shadow","inset 0 0 0 #000000")
+				var thei = this.id.split("_")[1];
+				var thej = this.id.split("_")[2];
+
+				pos = $("#stackedBarchart").offset();
+				thex = e.pageX - pos.left;
+				they = e.pageY - pos.top;
+				if (thex > $("#stackedBarchart").width() - 200) { thex = $("#stackedBarchart").width() - 200 };
+				if (they > $("#stackedBarchart").height() - 200) { they = $("#stackedBarchart").height() - 100 };
+						
+
+				for (var i in data) {
+					$("#id_"+i+"_"+thej).css("-moz-box-shadow","inset 0 0 3px #000000").css("-webkit-box-shadow","inset 0 0 3px #000000").css("box-shadow","inset 0 0 3px #000000")
+				}
+				$("#stackedBarchart").append('<div id="tag" style="top:'+they+'px; left:'+thex+'px;"><div class="category">'+nameNodes[thej].split("_").join(" ")+'</div><div class="valueNum">'+prefix+data[thei][nameNodes[thej]]+suffix+'</div></div>')
+			})
+		} else {
+			$('.portion').mouseover(function(){
+    			showTag(this.id)
+    		})
+		}
+   		
     	$('.portion').mouseout(function(){
     		$('#tag').remove();
     	})
